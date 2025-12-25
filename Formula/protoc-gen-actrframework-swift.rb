a4d6f10444fcdd10570378f75013b578e0f91a99
Harry <128583280+kaito-harry@users.noreply.github.com>
2025-12-25 13:58:50 +0800

    update protoc-gen-actrframework-swift to 0.1.2

--- Formula/protoc-gen-actrframework-swift.rb
+++ Formula/protoc-gen-actrframework-swift.rb
@@ -1,9 +1,9 @@
 class ProtocGenActrframeworkSwift < Formula
   desc "Swift protoc plugin for generating ACTR framework stubs"
   homepage "https://github.com/actor-rtc/framework-codegen-swift"
-  version "0.1.1"
+  version "0.1.2"
   url "https://github.com/actor-rtc/framework-codegen-swift/releases/download/v#{version}/protoc-gen-actrframework-swift-macos-arm64.zip"
-  sha256 "5cde2c53c928ba319143e36c18888cc7426e2c0d946c994a2f7ab1ba9e8e700c"
+  sha256 "be28d7b92822b2aba08ae35f28b04aa996b932e9b350d04efef8762e68ab9445"
 
   depends_on arch: :arm64
   depends_on macos: :ventura
